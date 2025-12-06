b15fac984f62f4cc00cafdb9974a5193f64b76a5
Enzo Ballardin <enzoballardinarcaro@gmail.com>
2025-12-06 15:27:57 -0300

    fix(deps): upgrade d3 from v4 to v7 to fix d3-color ReDoS vulnerability
    BREAKING CHANGE: Updated d3 from ^4.13.0 to ^7.9.0
    - Replaced d3.schemeCategory20c with d3.schemeTableau10 (removed in d3 v5)
    - d3-color is now 3.1.0 (fixes CVE for ReDoS vulnerability)
    - Version bumped to 3.0.0

--- src/react-bubble-chart-d3.js
+++ src/react-bubble-chart-d3.js
@@ -51,7 +51,7 @@ export default class BubbleChart extends Component {
       ? width * (1 - legendPercentage / 100)
       : width;
     const legendWidth = width - bubblesWidth;
-    const color = d3.scaleOrdinal(d3.schemeCategory20c);
+    const color = d3.scaleOrdinal(d3.schemeTableau10);
 
     const pack = d3
       .pack()
